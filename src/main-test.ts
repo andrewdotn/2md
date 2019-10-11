c01b66c63f96587a6b41d1a838f3dbf18a7b018b
Andrew Neitsch <andrew@neitsch.ca>
2019-10-11 16:47:41 -0600

    Prettier

--- src/main-test.ts
+++ src/main-test.ts
@@ -1,61 +1,72 @@
-import { randomBytes } from 'crypto';
-import { resolve } from 'path';
-import { expect } from 'chai';
-import {readFile } from 'fs-extra';
-import { run } from './main';
-import {toMd, A, B, Doc, H, L, P, parse} from "./2md";
+import { randomBytes } from "crypto";
+import { resolve } from "path";
+import { expect } from "chai";
+import { readFile } from "fs-extra";
+import { run } from "./main";
+import { toMd, A, B, Doc, H, L, P, parse } from "./2md";
 
-describe('run', function() {
-    it('raises an error if the command does not exist', async function() {
-        const dummyCommand = randomBytes(10).toString('hex');
-        let thrown = false;
-        let error: Error = new Error();
-        try {
-            await run([dummyCommand]);
-        } catch (e) {
-            thrown = true;
-            error = e;
-        }
-        expect(thrown).to.eql(true);
-        expect(/ENOENT/.test(error.toString())).to.eql(true);
-    });
+describe("run", function() {
+  it("raises an error if the command does not exist", async function() {
+    const dummyCommand = randomBytes(10).toString("hex");
+    let thrown = false;
+    let error: Error = new Error();
+    try {
+      await run([dummyCommand]);
+    } catch (e) {
+      thrown = true;
+      error = e;
+    }
+    expect(thrown).to.eql(true);
+    expect(/ENOENT/.test(error.toString())).to.eql(true);
+  });
 });
 
 async function fixture(filename: string): Promise<string> {
-    const path = resolve(__dirname, '../fixtures', filename);
-    const contents = await readFile(path);
-    return contents.toString();
+  const path = resolve(__dirname, "../fixtures", filename);
+  const contents = await readFile(path);
+  return contents.toString();
 }
 
-describe('2md', function() {
-    describe('parsing', function() {
-        it('can parse the first sample', async function() {
-            const html = await fixture('quote1.html');
-            const expected = new Doc(
-                [new H(['The end of 32-bit apps (and other removals)']),
-                new P(["Mac hardware and macOS made the jump from 32 bits to 64 bits a long time ago, but Catalina will be the very first version of macOS that is totally unable to run 32-bit software. For (what I hope will be) the last time, let's review the Mac's entire 64-bit timeline from start to finish:"]),
-                new L([new B(['June 2003']),
-                    ': The PowerPC G5 CPU is ',
-                    new A(['the first 64-bit-capable chip to show up in a Mac'],
-                        {href: 'https://www.macworld.com/article/1025078/future.html'}),
-                    ' and with Mac OS X 10.3 Panther, it can theoretically address up to 8GB of RAM.']),
-                        new L([new B(['April 2005']),
-                            'Mac OS X 10.4 Tiger ',
-                                new A(['allows for 64-bit processes under the hood'], {href: 'https://arstechnica.com/gadgets/2005/04/macosx-10-4/4/'}),
-                            '—they can be spun off from another process or run via the Terminal.'])]);
-            // Not implemented yet
-            this.skip();
-            expect(parse(html)).to.deep.equal(expected);
-        });
+describe("2md", function() {
+  describe("parsing", function() {
+    it("can parse the first sample", async function() {
+      const html = await fixture("quote1.html");
+      const expected = new Doc([
+        new H(["The end of 32-bit apps (and other removals)"]),
+        new P([
+          "Mac hardware and macOS made the jump from 32 bits to 64 bits a long time ago, but Catalina will be the very first version of macOS that is totally unable to run 32-bit software. For (what I hope will be) the last time, let's review the Mac's entire 64-bit timeline from start to finish:"
+        ]),
+        new L([
+          new B(["June 2003"]),
+          ": The PowerPC G5 CPU is ",
+          new A(["the first 64-bit-capable chip to show up in a Mac"], {
+            href: "https://www.macworld.com/article/1025078/future.html"
+          }),
+          " and with Mac OS X 10.3 Panther, it can theoretically address up to 8GB of RAM."
+        ]),
+        new L([
+          new B(["April 2005"]),
+          "Mac OS X 10.4 Tiger ",
+          new A(["allows for 64-bit processes under the hood"], {
+            href: "https://arstechnica.com/gadgets/2005/04/macosx-10-4/4/"
+          }),
+          "—they can be spun off from another process or run via the Terminal."
+        ])
+      ]);
+      // Not implemented yet
+      this.skip();
+      expect(parse(html)).to.deep.equal(expected);
     });
+  });
 
-    describe('end-to-end', function () {
-        it('can process the first sample', async function () {
-            const [html, md] = await Promise.all(['html', 'md'].map(ext =>
-                fixture(`quote1.${ext}`)));
-            // Not implemented yet
-            this.skip();
-            expect(toMd(html)).to.eql(md);
-        });
+  describe("end-to-end", function() {
+    it("can process the first sample", async function() {
+      const [html, md] = await Promise.all(
+        ["html", "md"].map(ext => fixture(`quote1.${ext}`))
+      );
+      // Not implemented yet
+      this.skip();
+      expect(toMd(html)).to.eql(md);
     });
+  });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,62 @@
-import {spawn} from 'child_process';
-import { inspect } from 'util';
-import {toMd} from "./2md";
+import { spawn } from "child_process";
+import { inspect } from "util";
+import { toMd } from "./2md";
 
-export function run(cmd: string[]): Promise<{stdout: string, stderr: string}> {
-    const proc = spawn(cmd[0], cmd.slice(1), {
-        stdio: ['ignore', 'pipe', 'pipe']
+export function run(
+  cmd: string[]
+): Promise<{ stdout: string; stderr: string }> {
+  const proc = spawn(cmd[0], cmd.slice(1), {
+    stdio: ["ignore", "pipe", "pipe"]
+  });
+
+  const output = { stdout: "", stderr: "" };
+
+  proc.stdio[1]!.on("data", data => (output.stdout += data));
+  proc.stdio[2]!.on("data", data => (output.stderr += data));
+
+  return new Promise((resolve, reject) => {
+    proc.on("error", e => reject(e));
+    proc.on("exit", (code, signal) => {
+      if (code === 1 && /\(-1700\)$/m.test(output.stderr)) {
+        return reject(
+          new Error(
+            "The clipboard does not currently contain HTML-formatted data."
+          )
+        );
+      }
+      if (code !== 0 || signal) {
+        return reject(
+          new Error(
+            `${inspect(
+              cmd
+            )} returned [${code}, ${signal}]; output was ${inspect(output)}`
+          )
+        );
+      }
+
+      return resolve(output);
     });
-
-    const output = {stdout: '', stderr: ''};
-
-    proc.stdio[1]!.on('data', data => output.stdout += data);
-    proc.stdio[2]!.on('data', data => output.stderr += data);
-
-    return new Promise((resolve, reject) => {
-        proc.on('error', e => reject(e));
-        proc.on('exit', (code, signal) => {
-            if (code === 1 && /\(-1700\)$/m.test(output.stderr)) {
-                return reject(new Error("The clipboard does not currently contain HTML-formatted data."));
-            }
-            if (code !== 0 || signal) {
-                return reject(new Error(`${inspect(cmd)} returned [${code}, ${signal}]; output was ${inspect(output)}`));
-            }
-
-            return resolve(output);
-        });
-    });
+  });
 }
 
 async function main() {
-    const osaOutput = await run(['osascript', '-e', 'the clipboard as «class HTML»']);
-    const hexEncodedHtml = osaOutput.stdout;
-    const match = /^«data HTML((?:[0-9A-F]{2})+)»$\n/m.exec(hexEncodedHtml);
-    if (!match) {
-        throw new Error("Could not parse osascript output");
-    }
-    const html = Buffer.from(match[1], 'hex').toString();
-    console.log(toMd(html));
+  const osaOutput = await run([
+    "osascript",
+    "-e",
+    "the clipboard as «class HTML»"
+  ]);
+  const hexEncodedHtml = osaOutput.stdout;
+  const match = /^«data HTML((?:[0-9A-F]{2})+)»$\n/m.exec(hexEncodedHtml);
+  if (!match) {
+    throw new Error("Could not parse osascript output");
+  }
+  const html = Buffer.from(match[1], "hex").toString();
+  console.log(toMd(html));
 }
 
 if (require.main === module) {
-    main().catch(e => {
-        console.error(e);
-        process.exit(1);
-    });
+  main().catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
 }

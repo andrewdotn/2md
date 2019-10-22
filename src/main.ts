c817439eb7cd88c0fd134d72e02ccfdd34deaa15
Andrew Neitsch <andrew@neitsch.ca>
2019-10-22 15:41:04 -0600

    Checkpoint

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,7 @@
-import { spawn } from "child_process";
-import { inspect } from "util";
+import yargs from "yargs";
+import { run } from "./run";
 import { parse } from "./parse";
 import { Rendering } from "./render";
-
-export function run(
-  cmd: string[]
-): Promise<{ stdout: string; stderr: string }> {
-  const proc = spawn(cmd[0], cmd.slice(1), {
-    stdio: ["ignore", "pipe", "pipe"]
-  });
-
-  const output = { stdout: "", stderr: "" };
-
-  proc.stdio[1]!.on("data", data => (output.stdout += data));
-  proc.stdio[2]!.on("data", data => (output.stderr += data));
-
-  return new Promise((resolve, reject) => {
-    proc.on("error", e => reject(e));
-    proc.on("exit", (code, signal) => {
-      if (code === 1 && /\(-1700\)$/m.test(output.stderr)) {
-        return reject(
-          new Error(
-            "The clipboard does not currently contain HTML-formatted data."
-          )
-        );
-      }
-      if (code !== 0 || signal) {
-        return reject(
-          new Error(
-            `${inspect(
-              cmd
-            )} returned [${code}, ${signal}]; output was ${inspect(output)}`
-          )
-        );
-      }
-
-      return resolve(output);
-    });
-  });
-}
 
 export function toMd(html: string): string {
   const intermediate = parse(html);
@@ -50,25 +13,84 @@ export function toMd(html: string): string {
   return rendered.result;
 }
 
-async function main() {
-  // Getting the clipboard as HTML is relatively easy in swift:
-  // https://stackoverflow.com/a/36109230/14558 but it’s not obvious how to call
-  // that API from nodejs, though I’m sure a native extension could be written.
-  //
-  // https://github.com/sindresorhus/clipboardy shells out to pbpaste
-  // and electron calls into Chromium’s native clipboard access code
-  const osaOutput = await run([
-    "osascript",
-    "-e",
-    "the clipboard as «class HTML»"
-  ]);
-  const hexEncodedHtml = osaOutput.stdout;
-  const match = /^«data HTML((?:[0-9A-F]{2})+)»$\n/m.exec(hexEncodedHtml);
-  if (!match) {
-    throw new Error("Could not parse osascript output");
+type CommandRunner = (args: { [x: string]: unknown }) => Promise<void> | void;
+
+interface Command {
+  name: string;
+  description?: string;
+  configureArgv?: (yargs: yargs.Argv) => void;
+  run: CommandRunner;
+  default?: boolean;
+}
+
+const commands: Command[] = [
+  {
+    name: "paste",
+    async run(args: {}) {
+      // Getting the clipboard as HTML is relatively easy in swift:
+      // https://stackoverflow.com/a/36109230/14558 but it’s not obvious how to call
+      // that API from nodejs, though I’m sure a native extension could be written.
+      //
+      // https://github.com/sindresorhus/clipboardy shells out to pbpaste
+      // and electron calls into Chromium’s native clipboard access code
+      const osaOutput = await run([
+        "osascript",
+        "-e",
+        "the clipboard as «class HTML»"
+      ]);
+      const hexEncodedHtml = osaOutput.stdout;
+      const match = /^«data HTML((?:[0-9A-F]{2})+)»$\n/m.exec(hexEncodedHtml);
+      if (!match) {
+        throw new Error("Could not parse osascript output");
+      }
+      const html = Buffer.from(match[1], "hex").toString();
+      console.log(toMd(html));
+    },
+    default: true
   }
-  const html = Buffer.from(match[1], "hex").toString();
-  console.log(toMd(html));
+  // {
+  //   name:
+  // }
+];
+
+async function main() {
+  let commandToRun: CommandRunner | undefined = undefined;
+  let defaultCommand: CommandRunner | undefined = undefined;
+  let defaultCount = 0;
+  let yargsParser = yargs.strict().demandCommand(0, 1);
+  for (let c of commands) {
+    if (c.default) {
+      defaultCommand = c.run;
+      defaultCount++;
+    }
+
+    yargsParser = yargsParser.command(
+      c.name,
+      c.description || "foo",
+      args => {
+        if (c.configureArgv) {
+          return c.configureArgv(args);
+        } else {
+          return {};
+        }
+      },
+      () => {
+        commandToRun = commandToRun;
+      }
+    );
+  }
+
+  if (defaultCount != 1) {
+    throw new Error("Exactly one command must be specified as the default.");
+  }
+
+  const argv = yargsParser.argv;
+
+  if (commandToRun === undefined) {
+    commandToRun = defaultCommand;
+  }
+
+  commandToRun!(argv);
 }
 
 if (require.main === module) {
